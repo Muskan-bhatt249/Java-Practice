036352117af6012acfade1c0d721a45760f455c9
Muskan Bhatt <155374856+Muskan-bhatt249@users.noreply.github.com>
2025-07-20 19:28:05 +0530

    Create hasSpecialSubstring.java

---
+++ hasSpecialSubstring.java
@@ -0,0 +1,15 @@
+class substringk {
+    public boolean hasSpecialSubstring(String s, int k) {
+        int count=1;
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i)== s.charAt(i-1)){
+                count++;
+            }
+            else{
+                if(count==k) return true;
+                count = 1;
+            }
+        }
+     return count == k;   
+    }
+}
